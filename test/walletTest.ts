07dd9f02c0a63c9269cd7298bd5cd848524bd2a4
Michael Welnick <Michael@Michaels-MacBook-Pro.local>
2016-06-02 07:32:14 -0700

    test

--- test/walletTest.ts
+++ test/walletTest.ts
@@ -2,5 +2,5 @@ import {Wallet} from "../src/wallet";
 
 var wallet = new Wallet('scheme caution cabin snack squeeze busy lava duck bleak cement medal endless');
 console.log(wallet.getAddress(0).toString())
-wallet.getBalance()
+wallet.getBalance() 
 console.log(wallet.accountPrivKey)
