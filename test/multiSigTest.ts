27d37b8040d9a19d909edc68ffcd8a7cb3e01874
Michael Welnick <Michael@Michaels-MacBook-Pro.local>
2016-08-16 08:20:39 -0700

    started multisig

---
+++ test/multiSigTest.ts
@@ -0,0 +1,5 @@
+var bitcore = require('bitcore-lib');
+var HDPrivateKey = bitcore.HDPrivateKey;
+var HDPublicKey = bitcore.HDPublicKey;
+
+var pubKey = new bitcore.PublicKey('03a473275a750a20b7b71ebeadfec83130c014da4b53f1c4743fcf342af6589a38');
